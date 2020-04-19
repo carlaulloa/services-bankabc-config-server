4dd0621919add4a92c987d430f376625c739cec5
carlaulloa <carla.ulloa.lp@gmail.com>
2020-04-19 00:30:51 -0500

    feat: config server

---
+++ src/test/java/com/app/abc/config/server/ServicesBankabcConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.abc.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesBankabcConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
